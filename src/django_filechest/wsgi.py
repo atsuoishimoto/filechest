54443af4083e5abd4b34aacacdf22b6085902bf1
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2026-01-17 13:40:19 +0900

    restore wsgi.py

--- src/django_filechest/wsgi.py
+++ src/django_filechest/wsgi.py
@@ -9,23 +9,24 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
-from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_filechest.settings")
 
+application = get_wsgi_application()
 
-class FileChestWSGIHandler(WSGIHandler):
-    def __call__(self, environ, start_response):
-        ret = super().__call__(environ, start_response)
-        print(
-            environ["REQUEST_METHOD"],
-            environ["SCRIPT_NAME"],
-            environ["PATH_INFO"],
-            environ["QUERY_STRING"],
-            getattr(ret, "status_code", None),
-        )
-        return ret
-
-
-application = FileChestWSGIHandler()
+#from django.core.handlers.wsgi import WSGIHandler
+#
+#class FileChestWSGIHandler(WSGIHandler):
+#    def __call__(self, environ, start_response):
+#        ret = super().__call__(environ, start_response)
+#        print(
+#            environ["REQUEST_METHOD"],
+#            environ["SCRIPT_NAME"],
+#            environ["PATH_INFO"],
+#            environ["QUERY_STRING"],
+#            getattr(ret, "status_code", None),
+#        )
+#        return ret
+#
+#application = FileChestWSGIHandler()
